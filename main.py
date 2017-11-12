4ee25a78ac7486767ac2f03fefe9c5b2c4020ca5
Андрей Серебро <andy.silv@yandex.ru>
2017-11-12 20:18:14 +0300

    added imagenet inference

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import json
 import time
-import datetime
-import dateutil
-import hashlib
 import vk
 from vk.exceptions import VkAPIError
 from os.path import dirname, join
-import sys
 import os
+import errno
+from urllib import request
+import classify_image
 
 root_dir = dirname(__file__)
 data_dir = join(root_dir, "..", "data")
 TIME_TO_SLEEP = 0.35
+TEMP_DIR = ".tmp"
+
+
+def make_sure_path_exists(path):
+    try:
+        os.makedirs(path)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
 
 
 class VkScrapper:
     def __init__(self, vkapi):
         self.vkapi = vkapi
-        pass
+        self.keywords = {'mushroom', 'bolete', 'fungus'}
 
     def get_photos(self, user):
         albums_count = self.vkapi.photos.getAlbumsCount(user_id=user)
@@ -35,8 +43,35 @@ class VkScrapper:
             for p in photos:
                 if not ('long' in p and 'lat' in p):
                     continue
-                print("photo with address {} has geotag: lat = {}, lon = {}".format(p['src_xbig'], p['lat'], p['long']))
+
+                tags = self.classify_photo(self.tagger, p['src_xbig'])
+                if not self.keywords.isdisjoint(tags):
+                    print("photo with address {} seems to contain mushrooms and has geotag: lat = {}, lon = {}".format(p['src_xbig'], p['lat'],
+                                                                                        p['long']))
+                    print(tags)
+                else:
+                    print(
+                        "photo with address {} has no mushrooms and has geotag: lat = {}, lon = {}".format(
+                            p['src_xbig'], p['lat'],
+                            p['long']))
             time.sleep(0.333)
+
+    def classify_photo(self, tagger, url):
+        make_sure_path_exists(TEMP_DIR)
+        image_path = join(TEMP_DIR, 'img.jpg')
+        f = open(image_path, 'wb')
+        f.write(request.urlopen(url).read())
+        f.close()
+
+        return tagger.run_inference_on_image(image_path)
+
+    def __enter__(self):
+        self.tagger = classify_image.ImageTagger('.models')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.tagger.close()
+
 
 def main():
     APP_ID = '6256422'
@@ -51,9 +86,10 @@ def main():
     session = vk.Session(access_token=tokens[0])
     vkapi = vk.API(session)
 
-    scrapper = VkScrapper(vkapi)
-    user = 9941496
-    scrapper.get_photos(user)
+    with VkScrapper(vkapi) as scrapper:
+        user = 9941496
+        scrapper.get_photos(user)
+
 
 if __name__ == '__main__':
     main()
